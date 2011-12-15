ed2f8924e42d9f3704d215f19bacad55bfcc62cf
Aniket <aniketn3@gmail.com>
2011-12-15 18:08:49 +0530

    Made major changes. DOM manipulation is now an extension through extension API. Made code more modular

--- tests/h5-tests.js
+++ tests/h5-tests.js
@@ -23,11 +23,23 @@ test("h5", function() {
    // $.html
    equals($("#innerHTML").html(), "innerHTML test", "$.html() test");
    
+   equals($("#innerHTML").html("<span>Hello World</span>").html().toLowerCase(), "<span>hello world</span>", "$.html(somehtml) test");
+   
    // $.attr
-   equals($(".attrTest").attr("class"), "attrTest", "$.attr() test");
+   equals($(".attrTest").attr("class"), "attrTest", "$.attr(name) test");
+   equals($(".attrTest").attr("class", "foo").hasClass("foo"), true, "$.attr(name, value) classname test");
+   equals($(".foo").attr("class", "foo").attr("class"), "foo", "$.attr(name, value) classname test");
+   equals($("#foo").attr("value", "voo").val(), "voo", "$.attr(name, value) input value test");
+   equals($("#foo").attr("value"), "voo", "$.attr(name, value) input value test");
+   
+   // $.data
+   equals($("#innerHTML").data("test", "testvalue").data("test"), "testvalue", "$.data(name, value) test");
+   
+   // $.append
+   equals($("#mytable").append("<tr id='foo'><td>Hello</td></tr>").find("#foo:first-child").html().toLowerCase(), "<td>hello</td>", "Table insertion test");
    
    // $.val
-   equals($("#foo").val(), "foo", "$.val() test");
+   equals($("#foo").val("foo").val(), "foo", "$.val() test");
    equals($("#foo").val("bar").val(), "bar", "$.val(value) test");
       // textarea
    equals($("#bar").val(), "Hello Bar", "textarea $.val() test");
@@ -39,6 +51,4 @@ test("h5", function() {
    equals($("#bazz").val().join(" "), "a b c", "$.val multiple select test");
    equals($("#bazz").val("c").val(), "c", "$.val multiple select test");
    equals($("#bazz").val(["b", "c"]).val().join(" "), "b c", "$.val multiple select set multiple values");
-   
-   equals($("#mytable").append("<tr id='foo'><td>Hello</td></tr>").find("#foo:first-child").html().toLowerCase(), "<td>hello</td>", "Table insertion test");
 });

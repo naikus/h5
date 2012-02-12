09136f419293921bd18a8fff4a57efb3c9b3dc62
Aniket <aniketn3@gmail.com>
2012-02-12 12:15:23 +0530

    Streamlined tests

--- tests/h5-tests.js
+++ tests/h5-tests.js
@@ -1,110 +1,151 @@
-module("nodelist");
+QUnit.config.autostart = false;
 
-test("Basic", function() {
-   expect(2);
-   ok(h5, "h5");
-   ok($, "$");
-});
-
-test("h5", function(){
-   var count = 0, arr;
+var AllTests = (function() {
+   function byId(id) {
+      return document.getElementById(id);
+   }
    
-   equals($().count(), 0, "$()");
-   equals($("").count(), 0, "$('')");
-   //raises(function() {$("#");},  "$('#') Throws Syntax Error"); fails in IE6 becacuse of CssQuery
-   equals($("span").selector, "span", "$(<selector>).selector === <selector>");
-   equals($($("span")).selector, "span", "$($(<selector>)).selector === <selector>");
-   
-   // context
-   ok($("#innerHTML", document.getElementById("qunit-fixture")), "$(selector, domcontext) test");
-   ok($("#innerHTML", $("#qunit-fixture")), "$(selector, $context) test");
-   
-   // $(html markup)
-   equals($("<p>Hello <span>World</span></p><div>Hi</div>").count(), 2, "$('<markup>') test");
-   equals($("<tr><td>Hello World</td></tr><tr><td>Yello World</td></tr>").html().toLowerCase(), 
-      "<td>hello world</td><td>yello world</td>", "$('<markup>') test");
-   
-   //forEach
-   $("#baz>option").forEach(function(e, i) {
-      console.log("option: " + e);
-      count++;
-   });
-   equals(count, 3, "$(selector).forEach(func) test");
-   count = 0;
-   
-   // filter
-   arr = $("option").filter(function(e, i) {
-      return $(e).attr("value") === "a";
-   });
-   console.log(arr);
-   equals(arr.length, 2, "$(selector).filter(funct) test");
-   
-   // $.get
-   equals($("div", $("#qunit-fixture")).get(0).nodeName.toLowerCase(), "div", "$.get() test");
-   
-   // $.forEach
-   var x = 0;
-   $.forEach([1,2,3,4,5], function(v) {
-       x += v;
-   });
-   equals(x, 15, "$.forEach() test");
-   
-   x = $.filter([1,2,3,4,5], function(v) {return v % 2 === 0;});
-   equals(x.join(","), "2,4", "$.filter() test");
-   
-});
-
-
-test("dom", function() {
-   // $.html
-   equals($("#innerHTML").html(), "innerHTML test", "$.html() test");
-   
-   equals($("#innerHTML").html("<span>Hello World</span><p>hey</p>").html().toLowerCase(), "<span>hello world</span><p>hey</p>", "$.html(somehtml) test");
-   
-   
-   // $.attr
-   equals($(".attrTest").attr("class"), "attrTest", "$.attr(name) test");
-   equals($(".attrTest").attr("class", "foo").hasClass("foo"), true, "$.attr(name, value) classname test");
-   equals($(".foo").attr("class", "foo").attr("class"), "foo", "$.attr(name, value) classname test");
-   equals($("#foo").attr("value", "voo").val(), "voo", "$.attr(name, value) input value test");
-   equals($("#foo").attr("value"), "voo", "$.attr(name, value) input value test");
-   
-   // $.append
-   equals($("#mytable").append("<tr id='foo'><td>Hello</td></tr>").find("#foo:first-child").html().toLowerCase(), "<td>hello</td>", "Table insertion test");
-   
-   // $.prepend
-   equals($("#mytable").prepend("<tr id='boo'><td>Yello</td></tr>").find("#boo:first-child").html().toLowerCase(), "<td>yello</td>", "Table insertion test");
-   
-   // $.val
-   equals($("#foo").val("foo").val(), "foo", "$.val() test");
-   equals($("#foo").val("bar").val(), "bar", "$.val(value) test");
-      // textarea
-   equals($("#bar").val(), "Hello Bar", "textarea $.val() test");
-   equals($("#bar").val("bar").val(), "bar", "textarea $.val(value) test");
-      // select
-   equals($("#baz").val(), "b", "select $.val() test");
-   equals($("#baz").val("c").val(), "c", "select $.val(value) test");
-   
-   equals($("#bazz").val().join(" "), "a b c", "$.val multiple select test");
-   equals($("#bazz").val("c").val(), "c", "$.val multiple select test");
-   equals($("#bazz").val(["b", "c"]).val().join(" "), "b c", "$.val multiple select set multiple values");
-   
-   // $.remove
-   $("#innerHTML").html("Hell<span>o World</span>, Yeah");
-   equals($("#innerHTML").remove("span").html(), "Hell, Yeah", "$(selector).remove() test");
-   
-   // $.hasClass
-   equals($(".clsTest").hasClass("clsTest"), true, "$(sel).hasClass(val) test");
-   
-   // $.addClass
-   equals($(".clsTest").addClass("bar").hasClass("bar"), true, "$(sel).addClass(val) test");
-   
-   // $.addClass
-   equals($(".clsTest").removeClass("bar").hasClass("bar"), false, "$(sel).removeClass(val) test");
-   
-   // $.data
-   equals($("#innerHTML").data("test", "testvalue").data("test"), "testvalue", "$.data(name, value) test");
-   
-   
-
-});
+   return {
+      run: function() {
+         /* ------------------------------------- core ------------------------------------------ */
+         module("h5.js");
+         test("h5 and $ object test", function() {
+            expect(3);
+            ok(h5, "h5 object exists as property of global object");
+            ok($, "$ alias exists as property of global object");
+            same($, h5, "$ and h5 are the same object");
+         });
+         
+         test("basic test", function() {
+            equals($().count(), 0, "$().count must be 0");
+            equals($("").count(), 0, "$('').count must be 0");
+         });
+         
+         test("Basic selector test", function() {
+            equals($("span").selector, "span", "$(<selector>).selector === <selector>");
+            equals($($("span")).selector, "span", "$($(<selector>)).selector === <selector>");
+         });
+         
+         test("Test selector passing context", function() {
+            same($(".innerHTML", byId("qunit-fixture")).get(0), byId("innerHTML"), 
+                  "$(selector, domcontext) with DOM element as context to $");
+            same($(".innerHTML", $("#qunit-fixture")).get(0), byId("innerHTML"), 
+                  "$(selector, $context) with another h5 object as context to $");
+         });
+         
+         test("$(selector).get(index) test", function() {
+            var divs = $("div", $("#qunit-fixture")); 
+            equals(divs.get(0).nodeName.toLowerCase(), "div", "$.get() test");
+            equals(divs.get(1).nodeName.toLowerCase(), "div", "$.get() test");
+         });
+         
+         test("$.html() test", function() {
+            equals($("<p>Hello <span>World</span></p><div>Hi</div>").count(), 2, 
+                  "$('<html>') test with html markup to $");
+            equals($("<tr><td>Hello World</td></tr><tr><td>Yello World</td></tr>").html().toLowerCase(), 
+                  "<td>hello world</td><td>yello world</td>", "$('<html>').html() === <html> test");
+         });
+         
+         test("$(selector).forEach test", function() {
+            var counter = 0;
+            $("#forEach>option").forEach(function(e, i) {counter++;});
+            equals(counter, 3, "$.(selector).forEach(function) test");
+         });
+         
+         test("$(selector).filter test", function() {
+            var retVal = $("option").filter(function(e, i) {
+               return $(e).attr("value") === "a";
+            });
+            equals(retVal.length, 3, "$(selector).filter(funct) test");         
+         });
+         
+         test("$.forEach() test", function() {
+            var x = 0, arr = [];
+            $.forEach([1,2,3,4,5], function(v, i) {
+                  x += v;
+            });
+            equals(x, 15, "$.forEach(Array, function) test");
+            
+            $.forEach({a:"A", b:"B", c:"C"}, function(val, key) {
+               arr[arr.length] = val;
+            });
+            equals(arr.join(""), "ABC", "$.forEach(Object, function) test"); 
+         });
+         
+         test("$.filter() test", function() {
+            var arr = $.filter([1,2,3,4,5], function(v, i) {return v % 2 === 0;});
+            equals(arr.join(","), "2,4", "$.filter(Array, function) test");
+            
+            arr = $.filter({a:"A", b:"B", c:"C"}, function(v, k) {
+               return k === "a" || k === "c";
+            });
+            equals(arr.join(","), "A,C", "$.filter(Object, function) test");
+         });
+         
+         
+         /* ------------------------------------- dom ------------------------------------------- */
+         module("dom.js");
+         test("$(sel).html() test", function() {
+            equals($("#innerHTML").html(), "innerHTML test", "$.html() test");   
+            equals($("#innerHTML").html("<span>Hello World</span><p>hey</p>").html().toLowerCase(), 
+               "<span>hello world</span><p>hey</p>", "$.html(somehtml) test");
+         });
+         
+         test("$(sel).attr() test", function() {
+            equals($(".attrTest").attr("class"), "attrTest", "$(sel).attr(name) test");
+            equals($(".attrTest").attr("class", "foo").hasClass("foo"), true, 
+               "$(sel).attr('class', value).hasClass(value) test");
+            equals($(".foo").attr("class", "foo").attr("class"), "foo", 
+               "$(sel).attr('class') classname test");
+            equals($("#foo").attr("value", "voo").val(), "voo", 
+               "$(sel).attr('value', val).val() === val, input value test");
+            equals($("#foo").attr("value"), "voo", "$(sel).attr('name') input val test");
+         });
+         
+         test("$(sel).append() test", function() {
+            equals($("#mytable").append("<tr id='foo'><td>Hello</td></tr>")
+               .find("#foo:first-child").html().toLowerCase(), "<td>hello</td>", "Table insertion test");
+         });
+         
+         test("$(sel).prepend() test", function() {
+            equals($("#mytable").prepend("<tr id='boo'><td>Yello</td></tr>")
+               .find("#boo:first-child").html().toLowerCase(), "<td>yello</td>", "Table insertion test");
+         });
+         
+         test("$(sel).val() test", function() {
+            equals($("#foo").val("foo").val(), "foo", "$(sel).val() test");
+            equals($("#foo").val("bar").val(), "bar", "$(sel).val(value) test");
+            // textarea
+            equals($("#bar").val(), "Hello Bar", "$(sel).val() test for textarea");
+            equals($("#bar").val("bar").val(), "bar", "$(sel).val(value) test for textarea");
+            // select
+            equals($("#baz").val(), "b", "$(sel).val() test for select");
+            equals($("#baz").val("c").val(), "c", "$(sel).val(value) test for select");
+            equals($("#bazz").val().join(" "), "a b c", "$(sel).val() test for multiple select");
+            equals($("#bazz").val("c").val(), "c", "$(sel).val() test for multiple select");
+            equals($("#bazz").val(["b", "c"]).val().join(" "), "b c", 
+               "$(sel).val() test for multiple select set multiple values");
+         });
+         
+         test("$(sel).remove() test", function() {
+            $("#innerHTML").html("Hell<span>o World</span>, Yeah");
+            equals($("#innerHTML").remove("span").html(), "Hell, Yeah", 
+               "$(selector).remove(selector) test");
+         });
+         
+         test("Css class name tests", function() {
+            equals($(".clsTest").hasClass("clsTest"), true, "$(sel).hasClass(val) test");
+            equals($(".clsTest").addClass("bar").hasClass("bar"), true, "$(sel).addClass(val) test");
+            equals($(".clsTest").removeClass("bar").hasClass("bar"), false, "$(sel).removeClass(val) test");
+         });
+         
+         test("$(sel).data() test", function() {
+            equals($("#innerHTML").data("test", "testvalue").data("test"), 
+               "testvalue", "$(sel).data(name, value) test");
+         });
+         
+         // finally start QUnit
+         QUnit.start();
+      }
+   }; 
+})();

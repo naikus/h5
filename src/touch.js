f32ac45aa023d5575627a491828d2cb3a6f6f935
Aniket <aniketn3@gmail.com>
2012-02-21 17:57:07 +0530

    Refactored touch events according to defineEvent api

--- src/touch.js
+++ src/touch.js
@@ -1,71 +1,92 @@
 (function($) {
-   /**
-    * Add support touch related convenience events 
-    */
-   if(document.createTouch) {
-      $.defineEvent(function() {
-         var state = {}, timer;
-         
-         function hasMoved(x1, y1, x2, y2) {
-            return Math.abs(x2 - x1) > 30 || Math.abs(y2 - y1) > 30;
-         }
-         
-         $(document.body)
-            .on("touchstart",  function(te) {
-               var now = Date.now(), touch = te.touches[0], elapsed = now - (state.last || now), target = te.target;
-               clearTimeout(timer);
-               if(elapsed > 0 && elapsed < 250 && target === state.target) { // may be douple tap
-                  state.dblTap = true;
-               }else {
-                  state = {};
-                  state.x = touch.screenX;
-                  state.y = touch.screenY;
-                  state.last = now;
-                  state.target = target;
-                  timer = setTimeout(function() {
-                     if(state.moved) {return;}
-                     state.taphold = true;
-                     te.preventDefault();
-                     $(target).dispatch("taphold");
-                  }, 700);
-               }
-            })
-            .on("touchmove", function(te) {
-               var touch = te.changedTouches[0];
-               state.moved = state.moved || hasMoved(state.x, state.y, touch.screenX, touch.screenY);
-               if(state.moved) {
-                  clearTimeout(timer);
-               }
-            })
-            .on("touchend", function(te) {
-               var target = te.target;
-               clearTimeout(timer);
-               if(state.moved || state.taphold) {
-                  state = {};
-                  return;
-               }
-               if(state.dblTap) {
-                  if(state.target === target) {
-                     $(target).dispatch("dbltap");
-                     state = {};
-                  }else {
-                     $(target).dispatch("tap");
-                  }
-               }else {
-                  $(target).dispatch("tap");
-               }
-               te.preventDefault();
-            })
-            .on("touchenter", function(te) {})
-            .on("touchleave", function(te) {})
-            .on("touchcancel", function(te) {
-               clearTimeout(timer);
-            });
-      });
-   }else {
+   var state, timer;
+   if(!document.createTouch) {
       console.log("Your browser does not support touch on this device");
+      return;
    }
+   state = {};
+   
+   function movement(x1, y1, x2, y2) {
+      var dx = x1 - x2, dy = y1 - y2; 
+      return Math.abs(dx) > 30 || Math.abs(dy) > 30 ? {x: dx, y: dy} : null;
+   }
+   
+   function direction(delta) {
+      var x = delta.x, y = delta.y;
+   }
+
+   function ontouch(te) {
+      var touch, now, elapsed, target = te.target, type = te.type, mov, tgt;
+      switch(type) {
+         case "touchstart":
+            touch = te.touches[0];
+            now = Date.now(); 
+            elapsed = now - (state.last || now);
+            
+            if(elapsed > 0 && elapsed < 250 && state.target === target) {
+               state.dbltap = true;
+            }else {
+               state.x = touch.screenX;
+               state.y = touch.screenY;
+               state.last = now;
+               state.target = target;
+               timer = setTimeout(function() {
+                  if(state.moved) {
+                     return;
+                  }
+                  state.taphold = true;
+                  te.preventDefault();
+                  $(target).dispatch("taphold");
+               }, 750);
+            }
+            break;
+         case "touchmove":
+            touch = te.changedTouches[0];
+            state.moved = movement(touch.screenX, touch.screenY, state.x, state.y);
+            break;
+         case "touchend":
+            if(state.taphold) {
+               delete state.taphold;
+               return;
+            }
+            clearTimeout(timer);
+            touch = te.changedTouches[0];
+            mov = state.moved;
+            tgt = $(target);
+            if(mov) {
+               tgt.dispatch("swipe");
+               delete state.moved;
+            }else {
+               tgt.dispatch("tap");
+               
+               if(state.dbltap) {
+                  delete state.last;
+                  delete state.dbltap;
+                  if(state.target === target) {
+                     tgt.dispatch("dbltap");
+                     return;
+                  }
+               }
+            }
+            break;
+         case "touchcancel":
+            clearTimeout(timer);
+            break;
+      }
+   }
+   
+   $.defineEvent({
+      setup: function() {
+         $(document.body).on("touchstart", ontouch)
+            .on("touchmove", ontouch)
+            .on("touchend", ontouch)
+            .on("touchcancel", ontouch);
+      },
+      destroy: function() {
+         $(document.body).un("touchstart", ontouch)
+            .un("touchmove", ontouch)
+            .un("touchend", ontouch)
+            .un("touchcancel", ontouch);
+      }
+   });
 })(h5);
-
-
-

f22eb4c45a74d68b12da58d42e09b3c90c99f35b
Aniket Naik <aniketn3@gmail.com>
2015-12-06 12:15:39 +0530

    Determining event types depending on availability of touch events

--- src/touch.js
+++ src/touch.js
@@ -5,11 +5,40 @@
  * @author aniketn3@gmail.com 
  */
 
+(function($, undefined) {
+  // set the default user action event (tap in touch enabled browsers or fallback to click
+  var Events = {
+    tap: "tap",
+    taphold: "taphold",
+    dbltap: "dbltap",
+    touchstart: "touchstart",
+    touchend: "touchend",
+    touchmove: "touchmove",
+    touchcancel: "touchcancel"
+  };
+  
+  if(!("ontouchstart" in document.documentElement)) {
+    Events = {
+      tap: "click",
+      taphold: "mousedown",
+      dbltap: "dblcick",
+      touchstart: "mousedown",
+      touchend: "mouseup",
+      touchmove: "mousemove",
+      touchcancel: "touchcancel"
+    };
+  }
+  
+  $.EventTypes = Events;
+  console.log($.EventTypes);
+  
+})(h5);
+
 /**
  * Tap event definition
  */
 (function($, undefined) {
-   var state = {/* id, x, y, target */};
+   var state = {/* id, x, y, target */}, EventTypes = $.EventTypes;
    
    function clearState() {
       state.id = state.x = state.y = state.moved = state.target = undefined;
@@ -23,7 +52,7 @@
    function handler(te) {
       var type = te.type, touch, touches = te.touches, cTouches = te.changedTouches, target = te.target;
       switch(type) {
-         case "touchstart":
+         case EventTypes.touchstart:
             if(touches.length !== 1) {
                return;
             }
@@ -33,13 +62,13 @@
             state.y = touch.pageY;
             state.target = target;
             break;
-         case "touchmove":
+         case EventTypes.touchmove:
             touch = cTouches[0];
             if(!state.moved && touch.identifier === state.id) {
                state.moved = hasMoved(state.x, state.y, touch.pageX, touch.pageY);
             }
             break;
-         case "touchend":
+         case EventTypes.touchend:
             if(cTouches.length === 0 || state.moved) {
                clearState();
                return;
@@ -51,7 +80,7 @@
                $(target).dispatch("tap");
             }
             break;
-         case "touchcancel":
+         case EventTypes.touchcancel:
             clearState();
             break;
       }
@@ -60,12 +89,12 @@
    $.defineEvent({
       type: "tap",
       setup: function() {
-         $(document).on("touchstart", handler).on("touchmove", handler)
-            .on("touchend", handler).on("touchcancel", handler);
+         $(document).on(EventTypes.touchstart, handler).on(EventTypes.touchmove, handler)
+            .on(EventTypes.touchend, handler).on(EventTypes.touchcancel, handler);
       },
       destroy: function() {
-         $(document).un("touchstart", handler).un("touchmove", handler)
-            .un("touchend", handler).un("touchcancel", handler);
+         $(document).un(EventTypes.touchstart, handler).un(EventTypes.touchmove, handler)
+            .un(EventTypes.touchend, handler).un(EventTypes.touchcancel, handler);
       }
    });
 })(h5);
@@ -104,7 +133,7 @@
  * Tap hold event
  */
 (function($, undefined) {
-      var state = {/* moved, x, y */}, timer;
+      var state = {/* moved, x, y */}, timer, EventTypes = $.EventTypes;
    
    function hasMoved(x1, y1, x2, y2) {
       var dx = x1 - x2, dy = y1 - y2;
@@ -118,7 +147,7 @@
    function handler(te) {
       var type = te.type, target = te.target;
       switch(type) {
-         case "touchstart":
+         case EventTypes.touchstart:
             if(te.touches.length !== 1) {
                return;
             }            
@@ -130,15 +159,15 @@
                }
             }, 700);
             break;
-         case "touchmove":
+         case EventTypes.touchmove:
             if(!state.moved) {
                if(state.moved = hasMoved(state.x, state.y, te.pageX, te.pageY)) { // jshint ignore:line
                   clearTimeout(timer);
                }
             }
             break;
-         case "touchend":
-         case "touchcancel":
+         case EventTypes.touchend:
+         case EventTypes.touchcancel:
             /* falls through */
          default:
             clearTimeout(timer);
@@ -150,12 +179,14 @@
    $.defineEvent({
       type: "taphold",
       setup: function() {
-         $(document).on("touchstart", handler).on("touchmove", handler).on("touchend", handler)
-            .on("touchcancel", handler);
+         $(document).on(EventTypes.touchstart, handler).on(EventTypes.touchmove, handler)
+            .on(EventTypes.touchend, handler)
+            .on(EventTypes.touchcancel, handler);
       },
       destroy: function() {
-         $(document).un("touchstart", handler).un("touchmove", handler).un("touchend", handler)
-            .un("touchcancel", handler);
+         $(document).un(EventTypes.touchstart, handler).un(EventTypes.touchmove, handler)
+            .un(EventTypes.touchend, handler)
+            .un(EventTypes.touchcancel, handler);
       }
    });
 })(h5);
@@ -165,9 +196,9 @@
  * Swipe event
  */
 (function($, undefined) {
-   var state = {};
+   var state = {}, EventTypes = $.EventTypes;
    
-   /**
+   /*
     * Calculate the delta difference between two points (x1,y1) and (x2,y2)
     * @return A delta object {x: xdelta, y: ydelta} if the difference is more
     * than 30 pixels or null if its less. The values x and y can be -ve 
@@ -194,7 +225,7 @@
       var type = te.type, touches = te.touches, touch, target, m, evtData;
       
       switch(type) {
-         case "touchstart":
+         case EventTypes.touchstart:
             touches = te.touches;
             if(touches.length > 1) {
                return;
@@ -204,19 +235,19 @@
             state.x = touch.pageX;
             state.y = touch.pageY;
             break;
-         case "touchmove":
+         case EventTypes.touchmove:
             touches = te.changedTouches;
             touch = touches[0];
             if(touch.identifier === state.id && te.touches.length === 1) {
                state.movement = getMovement(touch.pageX, touch.pageY, state.x, state.y);
             }
             break;
-         case "touchend":
+         case EventTypes.touchend:
             touches = te.changedTouches;
             touch = touches[0];
             if(state.id === touch.identifier && (m = state.movement)) {
                evtData = m;
-               $(te.target).dispatch("swipe", evtData); // available as event.data
+               $(te.target).dispatch("swipe", evtData); // available as event.movement
                clearState();
             }
             break;
@@ -229,12 +260,14 @@
    $.defineEvent({
       type: "swipe",
       setup: function() {
-         $(document).on("touchstart", handler).on("touchmove", handler).on("touchend", handler)
-            .on("touchcancel", handler);
+         $(document).on(EventTypes.touchstart, handler).on(EventTypes.touchmove, handler)
+            .on(EventTypes.touchend, handler)
+            .on(EventTypes.touchcancel, handler);
       },
       destroy: function() {
-         $(document).un("touchstart", handler).un("touchmove", handler).un("touchend", handler)
-            .un("touchcancel", handler);
+         $(document).un(EventTypes.touchstart, handler).un(EventTypes.touchmove, handler)
+            .un(EventTypes.touchend, handler)
+            .un(EventTypes.touchcancel, handler);
       }
    });
 })(h5);
